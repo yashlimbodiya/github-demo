3354e36c5f65a490ba93befb3a6c07b8b0f62ebb
yashlimbodiya <65219027+yashlimbodiya@users.noreply.github.com>
2021-08-02 14:30:53 +0530

    math.py upload initial

---
+++ math.py
@@ -0,0 +1,19 @@
+# Addition
+def add(x,y):
+    return x+y
+
+# Subtract
+def subtract(x,y):
+    return x-y
+	
+#Multiply
+def multiply(x,y):
+    return x*y
+	
+#Divide
+def divide(x,y):
+    if (y==0):
+		return 0
+	else:
+		return x/y
+
